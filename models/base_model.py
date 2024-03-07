68a31820d3f59583a8f29763933936e409478d1a
Emmanuel <kcemmy03@gmail.com>
2024-03-07 12:23:29 +0100

    changed the import code

--- models/base_model.py
+++ models/base_model.py
@@ -3,8 +3,9 @@
 This module contains a class BaseModel that defines all common attributes
 and methods for other classes.
 """
-import uuid
 from datetime import datetime
+import uuid
+import models
 
 
 class BaseModel:
@@ -16,19 +17,20 @@ class BaseModel:
         """
         This is the constructor method
         """
-        str_format = "%Y-%m-%dT%H:%M:%S.%f"
+        str_fmt = "%Y-%m-%dT%H:%M:%S.%f"
         lst = ["updated_at", "created_at"]
         if kwargs:
             for key, value in kwargs.items():
                 if key != "__class__":
                     if key in lst:
-                        setattr(self, key, datetime.strptime(value, str_format))
+                        setattr(self, key, datetime.strptime(value, str_fmt))
                     else:
                         setattr(self, key, value)
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
+            models.storage.new(self)
 
     def __str__(self):
         """
@@ -41,6 +43,7 @@ class BaseModel:
         This method updates a public instance attr with the current datetime
         """
         self.updated_at = datetime.now()
+        models.storage.save()
 
     def to_dict(self):
         """
