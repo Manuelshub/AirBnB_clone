fee265fee8a9ea75425a30ca7b9929181205388a
Esseh12 <gbenmoese788@gmai.com>
2024-03-08 15:25:09 -0800

    adding file_Storage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -4,6 +4,7 @@ This module contains a class FileStorage that serializes instances
 to a JSON file and deserializes JSON file to instances:
 """
 from models.base_model import BaseModel
+from models.user import User
 import json
 import os
 
