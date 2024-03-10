10f2e09f0e3713299320770bcb16331bb910a0a2
Emmanuel <kcemmy03@gmail.com>
2024-03-10 21:54:59 +0100

    Fixed import issue

--- models/base_model.py
+++ models/base_model.py
@@ -4,7 +4,7 @@ This module contains a class BaseModel that defines all common attributes
 and methods for other classes.
 """
 from datetime import datetime
-import models
+from . import storage
 import uuid
 
 
@@ -30,7 +30,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            models.storage.new(self)
+            storage.new(self)
 
     def __str__(self):
         """
@@ -43,7 +43,7 @@ class BaseModel:
         This method updates a public instance attr with the current datetime
         """
         self.updated_at = datetime.now()
-        models.storage.save()
+        storage.save()
 
     def to_dict(self):
         """
